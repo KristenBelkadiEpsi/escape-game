a738485abc72de82497ab7739b3b982b85f0dc42
debian-kristen <debian-kristen@serveur-mail.kristen>
2023-10-10 14:00:24 +0200

    ajout du scoreboard + sa route

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-
-const  routes = [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/salleRepos',
-      name: 'Salle de Repos',
-      component: () => import('../views/BreakRoomView.vue')
-    },
-    {
-      path: '/bureauRH',
-      name: 'Bureau des HR',
-      component: () => import('../views/HROfficeView.vue')
-    },
-    {
-      path: '/bureauPDG',
-      name: 'Bureau du PDG',
-      component: () => import('../views/OfficeCEOView.vue')
-    },
-    {
-      path: '/salleReunion',
-      name: 'Salle de reunion',
-      component: () => import('../views/MeetingRoomView.vue')
-    },
-    {
-      path: '/reglesJeu',
-      name: 'Régles du jeu',
-      component: () => import('../views/GameRulesView.vue')
-    }
-
-    
-    
-  ]
-
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/salleRepos",
+    name: "Salle de Repos",
+    component: () => import("../views/BreakRoomView.vue"),
+  },
+  {
+    path: "/bureauRH",
+    name: "Bureau des HR",
+    component: () => import("../views/HROfficeView.vue"),
+  },
+  {
+    path: "/bureauPDG",
+    name: "Bureau du PDG",
+    component: () => import("../views/OfficeCEOView.vue"),
+  },
+  {
+    path: "/salleReunion",
+    name: "Salle de reunion",
+    component: () => import("../views/MeetingRoomView.vue"),
+  },
+  {
+    path: "/reglesJeu",
+    name: "Régles du jeu",
+    component: () => import("../views/GameRulesView.vue"),
+  },
+  {
+    path: "/scoreboard",
+    name: "scoreboard",
+    component: () => import("../views/ScoreBoardView.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-  })
-  
-export default router
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/ScoreBoardView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app" class="container mt-5">
+    <div class="scoreboard-container">
+      <h1 class="scoreboard-header">Scoreboard</h1>
+      <table class="scoreboard-table">
+        <thead>
+          <tr>
+            <th>Utilisateur</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.nom }}</td>
+            <td>{{ user.score }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+const axiosInstance = axios.create({
+  baseURL: `http://127.0.0.1:8080`,
+});
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    await this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      axiosInstance.get("/user/utilisateurs").then((reponse) => {
+        this.users = reponse.data;
+      });
+    },
+  },
+};
+</script>
+<style>
+.scoreboard-container {
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.scoreboard-header {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.scoreboard-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+.scoreboard-table th,
+.scoreboard-table td {
+  border: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+}
+.scoreboard-table th {
+  background-color: #333;
+  color: #fff;
+}
+.scoreboard-table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+</style>
